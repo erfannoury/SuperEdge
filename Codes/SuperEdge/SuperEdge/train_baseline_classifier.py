3ceb7447c5d1a5b948d9a8afae880e0d6bf3dd40
Erfan Noury <erfannoury@users.noreply.github.com>
2015-05-30 20:55:30 +0430

    [Code] - changed lable type to int in train_baseline_classifier
    In the previous version of code in which labels were booleans,
    classifier wasn't trained properly. When loading the pickle file later,
    it didn't have property named `_enc` :frowning:

--- Codes/SuperEdge/SuperEdge/train_baseline_classifier.py
+++ Codes/SuperEdge/SuperEdge/train_baseline_classifier.py
@@ -38,7 +38,7 @@ def main():
     print 'starting training LinearSVM classifier'
     svc = LinearSVC()
     print 'LinearSVM Parameters: ', svc
-    svc.fit(features, (labels > 0))
+    svc.fit(features, int(labels > 0))
     print 'training LinearSVM classifier took: ', (datetime.now() - now)
     print 'saving trained classifier'
     f = file('../../../Models/linearSVC.pkl', 'wb')
