ef5f2c12b5aab0b0f11fa80cdcc3b6e5acdca185
Erfan Noury <erfannoury@users.noreply.github.com>
2015-06-05 01:11:50 +0430

    [Code] Updated test_baseline_classifier.py to use the new data loading implementation and save results with correct naming

--- Codes/SuperEdge/SuperEdge/test_baseline_classifier.py
+++ Codes/SuperEdge/SuperEdge/test_baseline_classifier.py
@@ -3,7 +3,8 @@ import cv2
 from extract_feature import VGG16Extractor
 from datetime import datetime
 from bsds500 import BSDS
-from sklearn.ensemble import RandomForestClassifier as RF
+#from sklearn.ensemble import RandomForestClassifier as RF
+import xgboost as xgb
 from matplotlib import pyplot as plt
 from scipy import io
 from sklearn.externals import joblib
@@ -11,46 +12,48 @@ from sklearn.externals import joblib
 def main():
     # load pre-trained model
     print 'loading pretrained model'
-    model_addr = '../../../Models/RandomForest.pkl'
-    rf = joblib.load(model_addr)
+    model_addr = '../../../Models/XGBR_10_d10.pkl'
+    clf = joblib.load(model_addr)
         
     now = datetime.now()    
     print 'loading test dataset'
-    Xtest, ytest = BSDS.load(which='test')
+    Xtest, ytest, ntest = BSDS.list_load(which='test')
     vgg = VGG16Extractor()
-    show_results = False
-    for i in xrange(Xtest.shape[0]):
+    show_results = True
+    save_results = False
+    for i in xrange(5):
         tic = datetime.now()
-        hyperimage = vgg.transform(Xtest[i,...])
-        print i, ' ', hyperimage.shape
-        ypred = rf.predict(hyperimage.reshape((hyperimage.shape[0] * hyperimage.shape[1], hyperimage.shape[2])))
+        hyperimage = vgg.transform(Xtest[i])
+        print str.format('%d - %s : ' % (i, ntest[i])), Xtest[i].shape
+        ypred = clf.predict(hyperimage.reshape((hyperimage.shape[0] * hyperimage.shape[1], hyperimage.shape[2])))
         ypred = ypred.reshape((hyperimage.shape[0], hyperimage.shape[1]))
+        ypred = (ypred - ypred.min()) / (ypred.max() - ypred.min())
 
         print 'single image prediction took: ', (datetime.now() - tic)
         if show_results:
             print 'displaying prediction result'
             plt.subplot(1,3,1)
-            plt.imshow(Xtest[i,...])
-            plt.title('Image')
+            plt.imshow(Xtest[i].astype(np.uint8))
+            plt.title(str.format('Image %s' % ntest[i]))
             plt.axis('off')
 
             plt.subplot(1,3,2)
             plt.imshow(ytest[i], cmap='gray')
-            plt.title('Ground truth')
+            plt.title(str.format('Ground truth %s' % ntest[i]))
             plt.axis('off')
 
             plt.subplot(1,3,3)
             plt.imshow(ypred, cmap='gray')
-            plt.title('Prediction')
+            plt.title(str.format('Prediction %s' % ntest[i]))
             plt.axis('off')
 
 
             plt.show()
-        else:
+        if save_results:
             print 'saving prediction result'
-            cv2.imwrite(str.format('../../../Datasets/BSDS500/result/test_%d.png' % i), (ypred*255))
-            cv2.imwrite(str.format('../../../Datasets/BSDS500/result/test_%d_gt.png' % i), (255 * ytest[i,...]))
-            io.savemat(str.format('../../../Datasets/BSDS500/result/test_%d.mat' % i), {'pred': ypred}, do_compression=True, appendmat=False)
+            cv2.imwrite(str.format('../../../Datasets/BSDS500/result/%s.png' % i), (ypred*255))
+            #cv2.imwrite(str.format('../../../Datasets/BSDS500/result/test_%d_gt.png' % i), (255 * ytest[i]))
+            io.savemat(str.format('../../../Datasets/BSDS500/result/%s.mat' % i), {'ucm2': ypred}, do_compression=True, appendmat=False)
 
         hyperimage = None
     print 'predicting output for test set took ', (datetime.now() - now)
