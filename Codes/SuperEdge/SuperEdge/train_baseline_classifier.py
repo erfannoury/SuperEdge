7118b12a2efe358021dffe57d1d3251c8614c85f
erfannoury <erfannoury@github.com>
2015-06-13 18:18:35 +0430

    Latest version of baseline classifier training implementation

--- Codes/SuperEdge/SuperEdge/train_baseline_classifier.py
+++ Codes/SuperEdge/SuperEdge/train_baseline_classifier.py
@@ -4,18 +4,18 @@ from extract_feature import VGG16Extractor
 from poisson_disc import PoissonDiskSampler
 from datetime import datetime
 from bsds500 import BSDS
-from sklearn.ensemble import RandomForestClassifier as RF
 from sklearn.externals import joblib
+import xgboost as xgb
 
 def main():
     now = datetime.now()    
     vgg = VGG16Extractor()
-    pds = PoissonDiskSampler(vgg.image_width, vgg.image_height, 4)
+    pds = PoissonDiskSampler(vgg.image_width, vgg.image_height, 2)
     samples = pds.get_sample()
     Xtrain, ytrain = BSDS.load(which='train')
     features = []
     labels = []
-    for i in xrange(Xtrain.shape[0]):
+    for i in xrange(50):
         hyperimage = vgg.transform(Xtrain[i,...])
         print i, ' ', hyperimage.shape
         for y in xrange(hyperimage.shape[0]):
@@ -35,13 +35,13 @@ def main():
     print 'labels.shape: ', labels.shape
 
     now = datetime.now()
-    print 'starting training RandomForest classifier'
-    rf = RF(n_jobs=12)
-    print 'RandomForest Parameters: ', rf
-    rf.fit(features, (labels > 0).astype(np.int32))
-    print 'training RandomForest classifier took: ', (datetime.now() - now)
-    print 'saving trained classifier'
-    joblib.dump(rf, '../../../Models/RandomForest.pkl', compress=True)
+    print 'starting training XGBoost regressor'
+    clf = xgb.XGBRegressor(max_depth=10, nthread=12, min_child_weight=2)
+    print 'XGBoost regressor Parameters: ', clf
+    clf.fit(features, labels)
+    print 'training xgboost regressor took: ', (datetime.now() - now)
+    print 'saving trained regressor'
+    joblib.dump(clf, '../../../Models/XGBR.pkl', compress=True)
 
 
 if __name__ == '__main__':
