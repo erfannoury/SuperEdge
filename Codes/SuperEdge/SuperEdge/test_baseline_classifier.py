eb4e189c3d84b7a876d8ca2371d791b013fe79cc
Erfan Noury <erfannoury@users.noreply.github.com>
2015-06-13 18:18:21 +0430

    Updated baseline classifier test implementation

--- Codes/SuperEdge/SuperEdge/test_baseline_classifier.py
+++ Codes/SuperEdge/SuperEdge/test_baseline_classifier.py
@@ -8,12 +8,13 @@ import xgboost as xgb
 from matplotlib import pyplot as plt
 from scipy import io
 from sklearn.externals import joblib
+import cv2
 import xgboost as xgb
 
 def main():
     # load pre-trained model
     print 'loading pretrained model'
-    model_addr = '../../../Models/XGBR_10_d10.pkl'
+    model_addr = '../../../Models/XGBR_50_d10_r2.pkl'
     clf = joblib.load(model_addr)
         
     now = datetime.now()    
@@ -24,12 +25,17 @@ def main():
     save_results = True
     for i in xrange(15):
         tic = datetime.now()
-        hyperimage = vgg.transform(Xtest[i])
+        print 'Xtest.shape = ', Xtest[i].shape
+        im = cv2.resize(Xtest[i], (2 * Xtest[i].shape[1], 2 * Xtest[i].shape[0]), interpolation=cv2.INTER_LINEAR)
+        print 'im.shape = ', im.shape
+        hyperimage = vgg.transform(im)
         print str.format('%d - %s : ' % (i, ntest[i])), Xtest[i].shape
         
         ypred = clf.predict(hyperimage.reshape((hyperimage.shape[0] * hyperimage.shape[1], hyperimage.shape[2])))
         ypred = ypred.reshape((hyperimage.shape[0], hyperimage.shape[1]))
         ypred = (ypred - ypred.min()) / (ypred.max() - ypred.min())
+        ypred = cv2.resize(ypred, (Xtest[i].shape[1], Xtest[i].shape[0]), interpolation=cv2.INTER_NEAREST)
+        print 'ypred.shape = ', ypred.shape
 
         print 'single image prediction took: ', (datetime.now() - tic)
         if show_results:
@@ -55,7 +61,7 @@ def main():
             print 'saving prediction result'
             cv2.imwrite(str.format('../../../Datasets/BSDS500/result/%s.png' % ntest[i]), (ypred * 255))
             #cv2.imwrite(str.format('../../../Datasets/BSDS500/result/test_%d_gt.png' % i), (255 * ytest[i]))
-            io.savemat(str.format('../../../Datasets/BSDS500/result/%s.mat' % ntest[i]), {'ucm2': ypred}, do_compression=True, appendmat=False)
+            #io.savemat(str.format('../../../Datasets/BSDS500/result/%s.mat' % ntest[i]), {'ucm2': ypred}, do_compression=True, appendmat=False)
 
         hyperimage = None
     print 'predicting output for test set took ', (datetime.now() - now)
