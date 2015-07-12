b4bbb83842721df7d7e8778e16aaf2362e76754b
Erfan Noury <erfannoury@users.noreply.github.com>
2015-07-12 12:07:56 +0430

    [Code] Updated train_classifier.py to remove the severe bug that was also present in the ondisk training implementation

--- Codes/SuperEdge/SuperEdge/train_classifier.py
+++ Codes/SuperEdge/SuperEdge/train_classifier.py
@@ -1,4 +1,4 @@
-import numpy as np
+﻿import numpy as np
 from extract_feature import VGG16Extractor
 from poisson_disc import PoissonDiskSampler
 from datetime import datetime
@@ -8,19 +8,19 @@ import xgboost as xgb
 import os.path as path
 
 def main():
-    cache_path = 'cache/'
+    cache_path = 'largecache/'
     feat_name = 'feat.dat'
     lbl_name = 'lbl.dat'
-    feat_len = 1088 #4224
+    feat_len = 4224 #1088
     now = datetime.now()
-    lbl_memmap = np.memmap(path.join(cache_path, lbl_name), dtype='float32', mode='r')
+    lbl_memmap = np.memmap(path.join(cache_path, lbl_name), dtype='uint8', mode='r')
     feat_memmap = np.memmap(path.join(cache_path, feat_name), dtype='float32', mode='r', shape=(lbl_memmap.shape[0], feat_len))
     print 'loading dataset took ', (datetime.now() - now)
     now = datetime.now()
     print 'starting training XGBoost classifier'
     clf = xgb.XGBClassifier(max_depth=20, nthread=24, n_estimators=150)
     print 'XGBoost classifier parameters: ', clf
-    clf.fit(features, (labels > 0).astype(uint8))
+    clf.fit(feat_memmap, lbl_memmap)
     print 'training xgboost classifier took: ', (datetime.now() - now)
     print 'saving trained classifier'
     joblib.dump(clf, '../../../Models/ondisk_20_XGBC.pkl', compress=True)
