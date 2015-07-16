891fc237a83a89901003fd151fd64dc943fc1b16
Erfan Noury <erfannoury@users.noreply.github.com>
2015-07-16 06:00:58 +0430

    [Code] Add external memory training using xgboost

---
+++ Codes/SuperEdge/SuperEdge/external_mem_train.py
@@ -0,0 +1,25 @@
+﻿import numpy as np
+import scipy.sparse
+import xgboost as xgb
+from sklearn.externals import joblib
+from os import path
+def main():
+    cache_path = 'cachelarge/'
+    dtrain_name = 'data.train.txt#dtrain.cache'
+    dtrain = xgb.DMatrix(path.join(cache_path, dtrain_name))
+    param = {'max_depth':15, 
+             'eta':1,
+             'learning_rate':0.1,
+             'gamma':0,
+             'silent':1,
+             'objective':'binary:logistic',
+             'n_estimators':50,
+             'nthread':24 }
+
+    bst = xgb.train(param, dtrain)
+    bst.save_model('../../../Models/external16_all_XGBC_d15_n50_int_th.xgb')
+
+
+
+if __name__ == '__main__':
+    main()
